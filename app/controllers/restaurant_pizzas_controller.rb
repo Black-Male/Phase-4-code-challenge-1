6b0b84d792d6e0582e1c4b121cc0b7a2f52df07a
Black-Male <Gkaranja20@gmail.com>
2023-03-21 12:23:18 +0300

    Validator added

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -6,12 +6,17 @@ class RestaurantPizzasController < ApplicationController
         restaurant_pizza = RestaurantPizza.create(myparams)
         # render json: restaurant_pizza, status: :created
         # render json: Pizza.find(RestaurantPizza.find_by(pizza_id: params[:pizza_id]))
-        render json: restaurant_pizza.pizza, only: [:id, :name, :ingredients], status: :created
+        if restaurant_pizza.valid?
+            render json: restaurant_pizza.pizza, only: [:id, :name, :ingredients], status: :created
+        else
+            render json: {message: restaurant_pizza.errors.full_messages}, status: :unprocessable_entity
+        end
     end
 
     private
 
     def myparams
         params.permit(:price, :restaurant_id, :pizza_id)
+
     end
 end
