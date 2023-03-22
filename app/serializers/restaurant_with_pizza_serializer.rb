ce40731ea9b88ebbb4bbea98a864ca738f757384
Black-Male <Gkaranja20@gmail.com>
2023-03-22 12:54:37 +0300

    Deployment test 9

--- app/serializers/restaurant_with_pizza_serializer.rb
+++ app/serializers/restaurant_with_pizza_serializer.rb
@@ -1,6 +1,4 @@
 class RestaurantWithPizzaSerializer < ActiveModel::Serializer
-  skip_before_action :verify_authenticity_token
-
   attributes :id,:name, :address
   has_many :pizzas, through: :restaurant_pizzas
 end
